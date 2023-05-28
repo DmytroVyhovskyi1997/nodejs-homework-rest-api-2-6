08e12d94db641dc53adf8ae60fa2ce2165ba0360
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-05-28 17:03:39 +0300

    add config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const DB_HOST =
+  "mongodb+srv://Dmytro:ghbctl100@cluster0.uuninah.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+  module.exports = {
+    DB_HOST,
+  }
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const app = require("./app");
-const mongoose = require('mongoose');
-const DB_HOST = 'mongodb+srv://Dmytro:ghbctl100@cluster0.uuninah.mongodb.net/contacts_reader?retryWrites=true&w=majority'
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const mongoose = require("mongoose");
+const {DB_HOST} = require('./config')
 
-mongoose.connect(DB_HOST)
-.then(()=> console.log("Database connect success"))
-.catch(error => console.log(error.message))
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });

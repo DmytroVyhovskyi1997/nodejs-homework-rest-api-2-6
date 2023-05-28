3f571d9f21f754d427a5fbe39f4d93b7f67dd855
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-05-28 16:38:37 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const app = require('./app')
-
+const app = require("./app");
+const mongoose = require('mongoose');
+const DB_HOST = 'mongodb+srv://Dmytro:ghbctl100@cluster0.uuninah.mongodb.net/contacts_reader?retryWrites=true&w=majority'
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
+
+mongoose.connect(DB_HOST)
+.then(()=> console.log("Database connect success"))
+.catch(error => console.log(error.message))

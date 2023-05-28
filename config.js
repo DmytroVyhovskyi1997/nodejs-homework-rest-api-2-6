bdfc0eafcf08f360c388bd52754b9a292690b443
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-05-28 20:50:50 +0300

    update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const DB_HOST =
   "mongodb+srv://Dmytro:ghbctl100@cluster0.uuninah.mongodb.net/contacts_reader?retryWrites=true&w=majority";
 
-  module.exports = {
-    DB_HOST,
-  }
+module.exports = {
+  DB_HOST,
+};

46a515d9d827115185ef89abb1f6099829ae02c0
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-05-28 21:05:29 +0300

    .

--- config.js
+++
@@ -1,6 +0,0 @@
-const DB_HOST =
-  "mongodb+srv://Dmytro:ghbctl100@cluster0.uuninah.mongodb.net/contacts_reader?retryWrites=true&w=majority";
-
-module.exports = {
-  DB_HOST,
-};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const {DB_HOST} = require('./config')
+const DB_HOST =
+  "mongodb+srv://Dmytro:ghbctl100@cluster0.uuninah.mongodb.net/contacts_reader?retryWrites=true&w=majority";
 
 mongoose
   .connect(DB_HOST)
